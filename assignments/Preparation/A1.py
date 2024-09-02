aaf3cd348af5e1b568ec401ed642945f8d8f16bf
20BCI0295 <iyashi.pal2020@vitstudent.ac.in>
2024-09-02 13:27:26 -0400

    A1.py

--- assignments/Preparation/A1.py
+++ assignments/Preparation/A1.py
@@ -1,4 +1,5 @@
-from sklearn.naive_bayes import GaussianNB
+#from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
 
 if __name__ == "__main__":
@@ -7,18 +8,31 @@ if __name__ == "__main__":
     
     # Explore the loaded pandas dataframe
     # Print out the 21st training data point
-    print(data_train.loc[20])
+    #print(data_train.loc[20])
+
+    #Print out the 12th training data point
+    print(data_train.loc[11])
+
     # Print out the column "Species"
-    print(data_train["Species"])
+    #print(data_train["Species"])
+
+    # Print out the column "SepalWidthCm"
+    print(data_train["SepalWidthCm"])
+
     # Print out the data points with "Species" == "Iris-setosa"
-    print(data_train[data_train["Species"]=="Iris-setosa"])
+    #print(data_train[data_train["Species"]=="Iris-setosa"])
+
+    #Print data points where SepalWidthCm<2.5
+    print(data_train[data_train["SepalWidthCm"]<2.5])
 
     # Separate independent variables and dependent variables
     independent = ["SepalLengthCm",	"SepalWidthCm",	"PetalLengthCm", "PetalWidthCm"]
     X = data_train[independent]
     Y = data_train["Species"]
+
     # Train model
-    clf = GaussianNB()
+    #clf = GaussianNB()
+    clf = DecisionTreeClassifier()
     clf.fit(X,Y)
     # Load testing data
     data_test = pd.read_csv("../data/Iris_test.csv")
