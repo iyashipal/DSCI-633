5cb46d81a037308472c68545fa40380f70227b03
20BCI0295 <iyashi.pal2020@vitstudent.ac.in>
2024-10-31 13:43:12 -0400

    A9

--- assignments/GradientDescent/my_Logistic.py
+++ assignments/GradientDescent/my_Logistic.py
@@ -24,12 +24,39 @@ class my_Logistic:
         self.w = np.array([0.0]*d)
         self.w0 = 0.0
         # write your code below
+        for epoch in range(self.max_iter):
+            # Optional shuffling for stochastic gradient descent
+            if self.shuffle:
+                indices = np.arange(n)
+                np.random.shuffle(indices)
+                data, y = data[indices], np.array(y)[indices]
+                
+            for i in range(0, len(X), self.batch_size):
+                X_batch = data[i:i + self.batch_size]
+                y_batch = y[i:i + self.batch_size]
+                
+                # Predict probabilities for the batch
+                linear_model = np.dot(X_batch, self.w) + self.w0
+                predictions = 1 / (1 + np.exp(-linear_model))
+                
+                # Gradient calculation
+                error = predictions - y_batch
+                gradient_w = np.dot(X_batch.T, error) / len(y_batch)
+                gradient_w0 = np.sum(error) / len(y_batch)
+                
+                # Update weights and bias
+                self.w -= self.learning_rate * gradient_w
+                self.w0 -= self.learning_rate * gradient_w0
 
     def predict_proba(self, X):
         # X: pd.DataFrame, independent variables
         # prob is a dict of prediction probabilities belonging to each categories
         # return probs = f(x) = 1 / (1+exp(-(w0+w*x))}); a list of float values in [0.0, 1.0]
         # write your code below
+        data = X.to_numpy()
+        # Calculate probabilities using sigmoid function
+        linear_model = np.dot(data, self.w) + self.w0
+        fx = 1 / (1 + np.exp(-linear_model))
         return fx
 
     def predict(self, X):
